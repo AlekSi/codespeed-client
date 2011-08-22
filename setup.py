128fe7a26e6ccd71cc5afcf43fee259252aca778
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-08-22 10:27:34 +0400

    Version 0.3.1 with trove classifiers.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='codespeed-client',
-    version='0.3.0',
+    version='0.3.1',
     author='Alexey Palazhchenko',
     author_email='alexey.palazhchenko@gmail.com',
     packages=['codespeed_client'],
@@ -13,4 +13,15 @@ setup(
     license='MIT',
     description='Library and command-line tool to push benchmark data to CodeSpeed.',
     long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Software Development :: Testing",
+        "Topic :: System :: Benchmark"]
 )
