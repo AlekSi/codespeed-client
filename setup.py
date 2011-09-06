66f201dc9cebadb7d2e5a871ebd1c64928aa84b2
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-09-06 19:22:00 +0400

    Version 0.3.2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='codespeed-client',
-    version='0.3.1',
+    version='0.3.2',
     author='Alexey Palazhchenko',
     author_email='alexey.palazhchenko@gmail.com',
     packages=['codespeed_client'],
